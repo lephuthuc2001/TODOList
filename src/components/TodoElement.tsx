799713553525c9c033eedbc35f3948707caf310c
Le Phu Thuc <lephuthuc2001@gmail.com>
2023-05-19 22:41:48 +0700

    Changed font-sizes and font-family to make RD.

--- src/components/TodoElement.tsx
+++ src/components/TodoElement.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import type { Todo } from "../models/Todo";
-import { Checkbox } from "antd";
+import { Checkbox, ConfigProvider } from "antd";
 import type { CheckboxChangeEvent } from "antd/es/checkbox";
 import { Button } from "antd";
 import { DeleteFilled } from "@ant-design/icons";
 import styles from "./Todo.module.scss";
 import useTodoStore from "../stores/TodoStore";
+import { useMediaQuery } from "react-responsive";
+import { Typography } from "antd";
 type Props = {
   todo: Todo;
 };
@@ -13,6 +15,12 @@ type Props = {
 function TodoElement({ todo }: Props) {
   const checkDone = useTodoStore((state) => state.checkDone);
   const removeTodo = useTodoStore((state) => state.removeTodo);
+
+  const isSm = useMediaQuery({ query: "(min-width: 576px)" });
+  const isMd = useMediaQuery({ query: "(min-width: 768px)" });
+  const isLg = useMediaQuery({ query: "(min-width: 992px)" });
+  const isXl = useMediaQuery({ query: "(min-width: 1200px)" });
+
   const onChange = (e: CheckboxChangeEvent) => {
     console.log(`checked = ${e.target.checked}`);
     checkDone(todo.id, e.target.checked);
@@ -22,18 +30,18 @@ function TodoElement({ todo }: Props) {
     <li className={styles.todo}>
       <div className={styles.todoName}>{todo.name}</div>
       <div className={styles.icons__group}>
-        <Checkbox
-          className={styles.todoName}
-          checked={todo.isDone}
-          onChange={onChange}
+        <ConfigProvider
+          componentSize={isLg ? "large" : isMd ? "middle" : "small"}
         >
-          Done
-        </Checkbox>
+          <Checkbox checked={todo.isDone} onChange={onChange}>
+            Done
+          </Checkbox>
+        </ConfigProvider>
         <Button
           onClick={() => {
             removeTodo(todo.id);
           }}
-          size="small"
+          size={isLg ? "middle" : "small"}
           icon={<DeleteFilled />}
         />
       </div>
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -2,14 +2,11 @@ import React from "react";
 import { Input, Button } from "antd";
 import { UnorderedListOutlined } from "@ant-design/icons";
 import { useForm, SubmitHandler, Controller } from "react-hook-form";
-import { useMediaQuery } from "react-responsive";
 import useTodoStore from "../stores/TodoStore";
 import type { Todo } from "../models/Todo";
 import { v4 as uuidv4 } from "uuid";
-// $sm: 576px;
-// $md: 768px;
-// $lg: 992px;
-// $xl: 1200px;
+import { useMediaQuery } from "react-responsive";
+
 import styles from "./TodoForm.module.scss";
 function TodoForm() {
   const addTodo = useTodoStore((state) => state.addTodo);
@@ -21,7 +18,7 @@ function TodoForm() {
   type FormValues = {
     todo: string;
   };
-  const { register, handleSubmit, control, reset } = useForm<FormValues>();
+  const { handleSubmit, control, reset } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = ({ todo }) => {
     const newTodo: Todo = {
